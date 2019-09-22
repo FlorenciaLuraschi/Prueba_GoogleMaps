dcf2df02e9e91073b3945059186ce3a4a01b8875
Florencia Luraschi <florencia.roxana.luraschi@gmail.com>
2019-09-21 21:28:28 -0300

    Agrega información/etiqueta al punto del marcador.

--- js/maps.js
+++ js/maps.js
@@ -3,13 +3,21 @@
 // Al inicio el mapa se verá centrado en Buenos Aires con un zoom de 8
 function initMap() {
   //Instanciando al mapa
-  var map = new google.maps.Map(document.getElementById('googleMap'), {
+  var mapa = new google.maps.Map(document.getElementById('googleMap'), {
     center: {lat: -34.603722, lng: -58.381592},
     zoom: 8
   });
   //Colocar una marca
-  var marker = new google.maps.Marker({
+  var marcador = new google.maps.Marker({
     position: {lat: -34.603722, lng: -58.381592},
-    map: map
+    map: mapa
+  });
+  //Agrega contenido/información al marcador
+  var contenido = new google.maps.InfoWindow({
+    content:'Prueba de texto'
+  });
+
+  marcador.addListener('click', function(){
+    contenido.open(mapa, marcador);
   });
 }
